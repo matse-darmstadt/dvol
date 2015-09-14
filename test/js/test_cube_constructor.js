d72e8baa02fdc822345a6c802fe7b78e744ecc51
jembel <j.belmokhtar@mgsoftech.com>
2015-09-14 22:25:56 +0200

    decided on a name for the cheese method js added air cube test

--- test/js/test_cube_constructor.js
+++ test/js/test_cube_constructor.js
@@ -3,8 +3,10 @@
  * 
  * These tests create cubes of different compositions and verify edibility  
  */
+ 
+// true on all segments
 QUnit.module ("Cube Constructor Tests");
-QUnit.test("Test GenerateCheese", function(assert) {
+QUnit.test("it's all cheese, man", function(assert) {
 	var boolStruct = GenerateCheese(3, function(x,y,z) {
 		return true;
 	});
@@ -12,4 +14,15 @@ QUnit.test("Test GenerateCheese", function(assert) {
 	assert.propEqual(boolStruct[0].length, 3, "checking cheese cube dimension");
 	assert.propEqual(boolStruct[0][0].length, 3, "checking cheese cube dimension");
 	assert.ok(boolStruct[0][0][0], "checking cheese cube value");
+});
+
+// false on all segments
+QUnit.test("a cube of air", function(assert) {
+	var boolStruct = GenerateCheese(3, function(x,y,z) {
+		return false;
+	});
+	assert.propEqual(boolStruct.length, 3, "checking cheese cube dimension");
+	assert.propEqual(boolStruct[0].length, 3, "checking cheese cube dimension");
+	assert.propEqual(boolStruct[0][0].length, 3, "checking cheese cube dimension");
+	assert.notOk(boolStruct[0][0][0], "checking cheese cube value");
 });
