4801bf385ac97388c51704fbca88e770ba296f7b
jembel <j.belmokhtar@mgsoftech.com>
2015-09-21 20:23:14 +0200

    added one more cube test + half the method tests; taking a break now

--- test/js/test_cheese_methods.js
+++ test/js/test_cheese_methods.js
@@ -3,14 +3,47 @@
  */
 QUnit.module ("other Tests");
 QUnit.test("parseBooleanStructure", function(assert) {
-	assert.ok(new Cheese().parseBooleanStructure != undefined,
-			"check if object.function exists")
+	var boolStruct = GenerateCheese(3, function(x,y,z) {
+		return true;
+	});
+	var parsedCheese = Cheese.parseBooleanStructure(boolStruct);
+	for (var z = 0; z < parsedCheese.length; z++) {
+		for (var y = 0; y < parsedCheese.length; y++) {
+			for (var x = 0; x < parsedCheese.length; x++) {
+				assert.ok(parsedCheese[z][y][x], "100% cheese");
+			}
+		}
+	};
 });
 QUnit.test("pour", function(assert) {
-	assert.ok(new Cheese().pour != undefined, "Exists")
+	var boolStruct = GenerateCheese(3, function(x,y,z) {
+		return true;
+	});
+	for (var y = 0; y < boolStruct.length; y++) {
+		for (var z = 0; z < boolStruct.length; z++) {
+			for (var x = 0; x < boolStruct.length; x++) {
+				if (boolStruct[z][y][x] === true) {
+					assert.ok(boolStruct.pour(z, x), "did flow, as expected")
+				}
+				else {
+					assert.notOk(boolStruct.pour(z, x), "didn't flow, as expected")
+				}
+				
+			}
+		}
+	};
 });
 QUnit.test("step", function(assert) {
-	assert.ok(new Cheese().step != undefined, "Exists")
+	var boolStruct = GenerateCheese(3, function(x,y,z) {
+		return true;
+	});
+	for (var z = 0; z < boolStruct.length; z++) {
+		for (var y = 0; y < boolStruct.length; y++) {
+			for (var x = 0; x < boolStruct.length; x++) {
+				assert.push(boolStruct.step(z, y, x), "stepped into cheese, eww!");
+			}
+		}
+	};
 });
 QUnit.test("getDiversions", function(assert) {
 	assert.ok(new Cheese().getDiversions != undefined, "Exists")
--- test/js/test_cube_constructor.js
+++ test/js/test_cube_constructor.js
@@ -26,3 +26,22 @@ QUnit.test("a cube of air", function(assert) {
 	assert.propEqual(boolStruct[0][0].length, 3, "checking cheese cube dimension");
 	assert.notOk(boolStruct[0][0][0], "checking cheese cube value");
 });
+
+// only top layer is cheese
+QUnit.test("a slice of cheese on an air cussion", function(assert) {
+	var boolStruct = GenerateCheese(3, function(x,y,z) {
+		if (z == 0) return true;
+		return false;
+	});
+	assert.propEqual(boolStruct.length, 3, "checking cheese cube dimension");
+	assert.propEqual(boolStruct[0].length, 3, "checking cheese cube dimension");
+	assert.propEqual(boolStruct[0][0].length, 3, "checking cheese cube dimension");
+	for (var z = 0; z < boolStruct.length; z++) {
+		for (var y = 0; y < boolStruct.length; y++) {
+			for (var x = 0; x < boolStruct.length; x++) {
+				if (z == 0) assert.ok(boolStruct[z][y][x], "a slice of cheese");
+				else assert.notOk(boolStruct[z][y][x], "just air");
+			}
+		}
+	};
+});
